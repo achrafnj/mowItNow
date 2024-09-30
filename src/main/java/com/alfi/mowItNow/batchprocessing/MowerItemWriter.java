9444295ec47ba859a65b5b720b34ebd6168f51fe
achrafnj <achraf.neji@outlook.com>
2024-09-30 15:15:12 +0200

    add profiles "dev" : local run and "prod" : docker run default profile : prod

--- src/main/java/com/alfi/mowItNow/batchprocessing/MowerItemWriter.java
+++ src/main/java/com/alfi/mowItNow/batchprocessing/MowerItemWriter.java
@@ -3,6 +3,9 @@ package com.alfi.mowItNow.batchprocessing;
 import com.alfi.mowItNow.model.Mower;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -12,10 +15,22 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class MowerItemWriter implements ItemWriter<List<Mower>> {
+    @Autowired
+    private Environment environment;
 
-    // docker output.txt file !!!
-    /*@Override
+    @Override
     public void write(Chunk<? extends List<Mower>> chunk) {
+        if (isDevProfileActive()) {
+            writeToFile(chunk);
+        } else {
+            writeToConsole(chunk);
+        }
+    }
+    private boolean isDevProfileActive() {
+        return environment.acceptsProfiles(Profiles.of("dev"));
+    }
+
+    public void writeToFile(Chunk<? extends List<Mower>> chunk) {
         String resourcesPath = Paths.get("src/main/resources").toAbsolutePath().toString();
         File outputFile = new File(resourcesPath, "output.txt");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, false))) {
@@ -28,10 +43,10 @@ public class MowerItemWriter implements ItemWriter<List<Mower>> {
         } catch (IOException e) {
             throw new RuntimeException("Error writing to output.txt file", e);
         }
-    }*/
+    }
 
-    @Override
-    public void write(Chunk<? extends List<Mower>> chunk) {
+
+    public void writeToConsole(Chunk<? extends List<Mower>> chunk) {
         int number = 0;
         for (List<Mower> mowers : chunk.getItems()) {
             System.out.println("=================");
